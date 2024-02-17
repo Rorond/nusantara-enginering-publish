08208a508937df629655badf9c5824257d69421c
muhammadroni <ronisam436@gmail.com>
2024-02-18 00:20:18 +0700

    Publish to github pages

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "245fefb4a0cc0ef0eee8d25320227a84",
-"index.html": "c984d1dde782166cbf4ef3f71c0c8fd7",
-"/": "c984d1dde782166cbf4ef3f71c0c8fd7",
-"main.dart.js": "cdce84d3ec70a9ca1b40e5442de62dbb",
+"index.html": "5e66a208778e242e325f920d8c5b7581",
+"/": "5e66a208778e242e325f920d8c5b7581",
+"main.dart.js": "4c703512bb87308f60c55b1c80430939",
 "flutter.js": "7d69e653079438abfbb24b82a655b0a4",
 "favicon.png": "4afb626310f8746b5341ccb766ebaacc",
 "icons/Icon-192.png": "c36d36b74f08c58265892b5ae500ab79",
